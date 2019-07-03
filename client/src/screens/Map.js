c083aa9d529fe72426da97941a83442f51f01f31
hektormisplon <hektor.misplon@gmail.com>
2019-07-03 09:51:26 +0200

    dependencies

---
+++ client/src/screens/Map.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Map extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  }
+
+  render() {
+    return <div>
+      <h1>Map</h1>
+    </div>
+  }
+}
