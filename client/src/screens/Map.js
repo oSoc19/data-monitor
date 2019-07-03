ddc1fc9f3fe07465732ebc5bacc961d98f98ca6d
hektormisplon <hektor.misplon@gmail.com>
2019-07-03 10:38:42 +0200

    innit

--- client/src/screens/Map.js
+++ client/src/screens/Map.js
@@ -1,14 +1,16 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 
 export default class Map extends Component {
   static propTypes = {
-    prop: PropTypes,
+    prop: PropTypes
   }
 
   render() {
-    return <div>
-      <h1>Map</h1>
-    </div>
+    return (
+      <div>
+        <h1>map</h1>
+      </div>
+    )
   }
 }
