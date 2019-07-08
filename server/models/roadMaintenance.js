be629e8e67a1d9c46ef133fddc03e4389298c90c
florent <florent.collin23@gmail.com>
2019-07-08 16:48:29 +0200

    Start modeling road maintenance for the database

---
+++ server/models/roadMaintenance.js
@@ -0,0 +1,44 @@
+const roadMaintenance = (sequelize, DataTypes) => {
+  const RoadMaintenance = sequelize.define('roadMaintenance', {
+    id: {
+      type: DataTypes.STRING,
+      unique: true
+    }
+    type: {
+      type: DataTypes.STRING
+    },
+    
+    source: {
+      type: DataTypes.STRING
+    },
+
+    startTime: {
+      type: DataTypes.DATE
+    },
+
+    endTime: {
+      type: DataTypes.DATE
+    }, 
+
+    speedManagementType: {
+      type: DataTypes.STRING
+    }, 
+
+    temporarySpeedLimit: {
+      type: DataTypes.FLOAT
+    }, 
+
+    mobility: {
+      type: DataTypes.STRING
+    }, 
+
+    roadMaintenanceType: {
+      type: DataTypes.STRING
+    } 
+
+  });
+
+  return RoadMaintenance;
+};
+
+export default roadMaintenance;
