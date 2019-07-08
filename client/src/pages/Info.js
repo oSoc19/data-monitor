1f444b67906429e77ead6ad97ad0885987f46af7
hektormisplon <hektor.misplon@gmail.com>
2019-07-08 15:54:35 +0200

    nav routing

---
+++ client/src/pages/Info.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a
+            href="//data.openparking.nl/index/index.php"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download latest parkeerdata index
+          </a>
+        </li>
+        <li>
+          <a
+            href="//data.openparking.nl/downloads/Gebruikshandleiding_openparking.nl_20190212.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gebruikshandleiding openparking.nl
+          </a>
+        </li>
+        <li>
+          <a
+            href="//data.openparking.nl/downloads/Handreiking_kwaliteitsverbetering_Parkeerdata_20190212.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Handreiking kwaliteitsverbetering Parkeerdata
+          </a>
+        </li>
+        <li>
+          <a
+            href="//data.openparking.nl/downloads/Standard_for_the_Publication_of_Dynamic_Parking_Data_v2.0.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Standard for the Publication of Dynamic Parking Data v2.0
+          </a>
+        </li>
+        <li>
+          <a
+            href="//data.openparking.nl/downloads/MuConsult_2018_Rapport_Maastchappelijke_Baten_Open_Parkeerinformatie.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rapport Maastchappelijke Baten Open Parkeerinformatie
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
