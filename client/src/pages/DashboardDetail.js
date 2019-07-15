cdff9793b94d126053f86b523213f7f464bc95a1
hektormisplon <hektor.misplon@gmail.com>
2019-07-15 16:38:59 +0200

    dashboard detail

---
+++ client/src/pages/DashboardDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardDetail = props => {
+  console.log(props)
+  return (
+    <div>
+      <h1>dashboard detail</h1>
+    </div>
+  )
+}
+
+export default DashboardDetail
