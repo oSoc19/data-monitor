26adde618cd13da08c8bb72aedec433444199333
hektormisplon <hektor.misplon@gmail.com>
2019-07-18 12:13:12 +0200

    initial state

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,55 @@
-import React from "react";
-import "./sass/main.sass";
+import React from 'react'
+import './sass/main.sass'
 
-import { StateProvider } from "./utilities/state";
+import { StateProvider } from './utilities/state'
 
-import LeafletMap from "./pages/LeafletMap";
-import MapOverview from "./pages/MapOverview";
-import Dashboard from "./pages/Dashboard";
+import LeafletMap from './pages/LeafletMap'
+import MapOverview from './pages/MapOverview'
+import Dashboard from './pages/Dashboard'
 
-import Home from "./pages/Home";
-import Info from "./pages/Info";
+import Home from './pages/Home'
+import Info from './pages/Info'
 
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom'
 
-import { bridgeOpenings } from "./config/api";
+import { bridgeOpenings } from './config/api'
 
 const App = () => {
   /**
    * Global state
    */
   const initialState = {
-    dataSet: { name: "bridges", summary: bridgeOpenings.summary, map: bridgeOpenings.map }
-  };
+    dataSet: {
+      name: 'bridges',
+      summary: bridgeOpenings.summary,
+      map: bridgeOpenings.map,
+      icon: 'bridge'
+    }
+  }
 
   const reducer = (state, action) => {
     switch (action.type) {
-      case "changeDataSet":
+      case 'changeDataSet':
         return {
           ...state,
           dataSet: action.newDataSet
-        };
+        }
       default:
-        return state;
+        return state
     }
-  };
+  }
 
   return (
     <React.Fragment>
-      <Route path="/" exact component={Home} />
-      <Route path="/leaflet" component={LeafletMap} />
+      <Route path='/' exact component={Home} />
+      <Route path='/leaflet' component={LeafletMap} />
       <StateProvider initialState={initialState} reducer={reducer}>
-        <Route path="/mapbox" component={MapOverview} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path='/mapbox' component={MapOverview} />
+        <Route path='/dashboard' component={Dashboard} />
       </StateProvider>
-      <Route path="/info" component={Info} />
+      <Route path='/info' component={Info} />
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default App;
+export default App
