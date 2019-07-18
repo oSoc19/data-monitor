5660d551387ceadb787fefb084cee45603682a63
hektormisplon <hektor.misplon@gmail.com>
2019-07-18 10:00:14 +0200

    api endpoint config

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,20 @@
-export default {
-  base: 'http://82.196.10.230',
+const api = {
+  domain: "http://82.196.10.230",
   port: 8080,
-  endPoint: '/api/'
-}
+  endPoint: "/api/"
+};
+
+const apiUrl = `${api.domain}:${api.port}${api.endPoint}`;
+
+export const bridgeOpenings = {
+  summary: `${apiUrl}qa/bridge_openings/summary`,
+  map: `${apiUrl}bridges`,
+  csv: `${apiUrl}download/bridge_openings/summary`,
+  modify: `${apiUrl}qa/bridge_openings/:id`
+};
+
+export const maintenanceWorks = {
+  summary: `${apiUrl}qa/maintenance_works/summary`,
+  map: `${apiUrl}maintenance_works/?startTime=2019-07-08T12:20:38.000Z&endTime=2019-07-09T12:20:38.000Z`,
+  csv: `${apiUrl}download/maintenance_works/summary`
+};
