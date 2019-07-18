d802ea472912ef035056c26ddc012cb367c0a514
hektormisplon <hektor.misplon@gmail.com>
2019-07-18 12:12:47 +0200

    styling

--- client/src/components/LayoutWrapper.js
+++ client/src/components/LayoutWrapper.js
@@ -7,7 +7,14 @@ const LayoutWrapper = props => {
   return (
     <div className='container'>
       <Header />
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          flexDirection: 'row'
+        }}
+      >
         <Sidebar />
         {props.children}
       </div>
