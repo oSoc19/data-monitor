00a6c6607fdd0aa7e7f299458ce9ce452c025b44
hektormisplon <hektor.misplon@gmail.com>
2019-07-22 11:09:31 +0200

    filter queries

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -10,13 +10,15 @@ export const bridgeOpenings = {
   summary: `${apiUrl}qa/bridge_openings/summary`,
   map: `${apiUrl}bridges`,
   csv: `${apiUrl}download/bridge_openings/summary`,
-  modify: `${apiUrl}qa/bridge_openings/:id`
+  modify: `${apiUrl}qa/bridge_openings/:id`,
+  query: ``
 }
 
 export const maintenanceWorks = {
   summary: `${apiUrl}qa/maintenance_works/summary`,
-  map: `${apiUrl}maintenance_works/?startTime=2019-07-08T12:20:38.000Z&endTime=2019-07-20T12:20:38.000Z`,
-  csv: `${apiUrl}download/maintenance_works/summary`
+  map: `${apiUrl}maintenance_works`,
+  csv: `${apiUrl}download/maintenance_works/summary`,
+  query: `/?startTime=2019-07-08T12:20:38.000Z&endTime=2019-07-20T12:20:38.000Z`
 }
 
 export const incidents = {
