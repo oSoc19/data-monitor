60b17c3d228805394e4c37faaeea084ab72412b9
hektormisplon <hektor.misplon@gmail.com>
2019-07-22 11:09:10 +0200

    tab styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,23 +2,31 @@ import React from 'react'
 import logo from '../../assets/logo.svg'
 import './Header.sass'
 
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 import { Map, Grid, Info } from 'react-feather'
 
 export const Header = () => {
   return (
     <div className='header'>
-      <NavLink to='/mapbox' className='header-brand'>
+      <Link to='/mapbox' className='header-brand'>
         <img className='header-brand_logo' src={logo} alt='' />
         <h4 className='header-brand_name'>Verkeersdatamonitor</h4>
-      </NavLink>
+      </Link>
       <div className='header-nav'>
-        <NavLink to='/mapbox' className='header-nav_link'>
+        <NavLink
+          to='/mapbox'
+          className='header-nav_link'
+          activeClassName='active'
+        >
           <Map />
           <span>Map</span>
         </NavLink>
-        <NavLink to='/dashboard' className='header-nav_link'>
+        <NavLink
+          to='/dashboard'
+          className='header-nav_link'
+          activeClassName='active'
+        >
           <Grid />
           Dashboard
         </NavLink>
