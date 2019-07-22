3f993ba8cb881eb0858bcc6a4327a6784643fc87
hektormisplon <hektor.misplon@gmail.com>
2019-07-22 22:58:50 +0200

    sass modules in component

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './utilities/serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
-import './sass/main.sass'
 
 ReactDOM.render(
   <BrowserRouter>
