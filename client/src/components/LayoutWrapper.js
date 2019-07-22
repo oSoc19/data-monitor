10b6e7f8f9874e3274c2fbc8421726b0b51650b7
hektormisplon <hektor.misplon@gmail.com>
2019-07-22 11:09:56 +0200

    flex direction prop

--- client/src/components/LayoutWrapper.js
+++ client/src/components/LayoutWrapper.js
@@ -12,7 +12,7 @@ const LayoutWrapper = props => {
           display: 'flex',
           height: '100%',
           width: '100%',
-          flexDirection: 'row'
+          flexDirection: props.direction
         }}
       >
         <Sidebar />
