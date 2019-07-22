aa8279f6321f7475fc80675e2001a280f19c0acd
hektormisplon <hektor.misplon@gmail.com>
2019-07-22 11:08:53 +0200

    detail no data

--- client/src/components/DashboardDetail/DashboardDetail.js
+++ client/src/components/DashboardDetail/DashboardDetail.js
@@ -5,7 +5,7 @@ import { Check, X } from 'react-feather'
 
 const DashboardDetail = props => {
   const { summary } = props
-  return (
+  return summary.length > 0 ? (
     <div>
       <table>
         <thead>
@@ -13,9 +13,12 @@ const DashboardDetail = props => {
             <td>Event Id</td>
             <td>Fields complete</td>
             <td>Correct Id</td>
-            <td>Checksum</td>
-            {/* <td>Manual intervention</td>
-          <td>Comment</td> */}
+            <td>Version</td>
+            <td>Probability of occurence</td>
+            <td>Source</td>
+            <td>Location for display</td>
+            <td>Location</td>
+            <td>General network management type</td>
           </tr>
         </thead>
         {summary.map(event => {
@@ -25,15 +28,20 @@ const DashboardDetail = props => {
                 <td>{event.bridgeOpeningId}</td>
                 <td>{event.allFields ? <Check /> : <X />}</td>
                 <td>{event.correctID ? <Check /> : <X />}</td>
-                <td>{event.checksum ? <Check /> : <X />}</td>
-                {/* <td>{event.manualIntervention ? <Check /> : <X />}</td>
-              <td>{event.comment}</td> */}
+                <td>{event.version}</td>
+                <td>{event.probabilityOfOccurence}</td>
+                <td>{event.source}</td>
+                <td>{event.locationForDisplay}</td>
+                <td>{event.location}</td>
+                <td>{event.generalNetworkManagementType}</td>
               </tr>
             </tbody>
           )
         })}
       </table>
     </div>
+  ) : (
+    <h4>No events for this city</h4>
   )
 }
 
