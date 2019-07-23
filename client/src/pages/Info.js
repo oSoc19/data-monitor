7a5afb1299e6670d2e9a79378240cef6a0f6d7dd
sidkha <ryan.byloos@student.umons.ac.be>
2019-07-23 09:50:50 +0200

    Improved about page

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -7,6 +7,13 @@ import LayoutWrapper from '../components/LayoutWrapper'
 const Info = () => {
   return (
     <LayoutWrapper direction='column'>
+      <h4>About</h4>
+      <p>Let’s suppose you own an autonomous car and you want to navigate to Amsterdam. You set up your car to calculate a route and start following the directions. On the way, the car system included the passage of a bridge as part of the route, but upon arrival you can’t cross it. This is because incorrect data has been sent to the car and hasn’t been changed ever since. With Verkeersdatamonitor problems like these are resolved. Accurate mobility data fixes the problem of traffic jams and unnecessary detours and makes way for a fluid traffic.</p>
+      <p>Verkeersdatamonitor is a web-based project for Dutch municipalities, provinces and the government, launched by the Dutch Ministry of Infrastructure and Water Management. The project is about monitoring the quality of mobility data about the Netherlands.</p>
+      <p>It shows a map with a sidebar by default. On the sidebar there is a possibility to toggle multiple datasets on and off. Apart from that you can also select a date range. The results show several pinpoints on the map.
+You can switch to a dashboard view, that’s divided in clickable tiles named after and ordered by the Dutch municipalities, regions and provinces. Each tile shows the data in a graph and with each click you can narrow down to the smallest part of the Netherlands. Finally it gives a detailed view of the dataset as a table. You can edit the data where needed and download a csv-file about this dataset.</p>
+      <p>The tests to monitor the quality of the data are mostly based on the completeness of the data and the correctness. So if we miss some data, or if it is not what it is supposed to be, then it is considered wrong.</p>
+      <p>Three datasets are implemented for now. "Bridge Openings", "Maintenance Works" (from "Road Works") and "Accidents" (from "Incidents"). More datasets from the NDW opendata should be implemented in the future.</p>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <h4>Summary endpoints</h4>
         <code>{bridgeOpenings.summary}</code>
@@ -17,10 +24,6 @@ const Info = () => {
         <h4>Modify endpoint</h4>
         <code>{bridgeOpenings.modify}</code>
       </div>
-      <h4>About</h4>
-      <p>
-        <p>Description goes here</p>
-      </p>
       <ul>
         <li>
           <a
@@ -28,7 +31,7 @@ const Info = () => {
             target='_blank'
             rel='noopener noreferrer'
           >
-            NDW
+            NDW website
           </a>
         </li>
       </ul>
